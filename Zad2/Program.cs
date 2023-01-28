d23b3ed0a89ed2f690919e590b97cfc41b604403
Pavel_Pichugin <pichps@mail.ru>
2023-01-28 07:06:15 +0300

    Zad2-finish

--- Zad2/Program.cs
+++ Zad2/Program.cs
@@ -2,4 +2,17 @@
 // какое число большее, а какое меньшее.
 // a = 5; b = 7 -> max = 7
 
-Console.WriteLine("Hello, World!");
+Console.WriteLine("Введите число-a: ");
+int numberA = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите число-b: ");
+int numberB = Convert.ToInt32(Console.ReadLine());
+
+if (numberA > numberB)
+{
+    Console.WriteLine($"max = {numberA}");
+}
+if (numberA < numberB)
+{
+    Console.WriteLine($"max = {numberB}");
+}
